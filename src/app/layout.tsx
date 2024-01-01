987ec7ca66ffbdff6a8af8288c1a0eb9cc2df816
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-01 12:09:26 +0600

    import font source robot in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,8 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
