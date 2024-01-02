e207ab669e85b5fea3f72a5614545d7975b4cd27
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-02 14:09:24 +0600

    return with router.push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/home");
-  return (
-    <main>
-      <Button variant="contained">Text</Button>
-    </main>
-  );
+  return router.push("/home");
 }
