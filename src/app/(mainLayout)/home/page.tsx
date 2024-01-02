48e7de5740d64363a516c4ad6bf4b961cb7a63fd
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-02 16:15:56 +0600

    home page FormDatePicker component

--- src/app/(mainLayout)/home/page.tsx
+++ src/app/(mainLayout)/home/page.tsx
@@ -1,7 +1,10 @@
+import FormDatePicker from "@/components/Form/FormDatePicker";
+
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <FormDatePicker />
     </div>
   );
 };
