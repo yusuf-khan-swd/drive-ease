bfe7d14836b9b5b66b48460d073834fbb5fb565f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-03 13:19:06 +0600

    Remove Dashboard Page

--- src/app/dashboard/new/page.tsx
+++
@@ -1,9 +0,0 @@
-const NewPage = () => {
-  return (
-    <div>
-      <h1>New Page</h1>
-    </div>
-  );
-};
-
-export default NewPage;
--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-const DashboardPage = () => {
-  return (
-    <div>
-      <h1>DashboardPage</h1>
-    </div>
-  );
-};
-
-export default DashboardPage;
