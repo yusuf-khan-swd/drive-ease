5ce04164bfba516909a442fbd11dd878cd822f9f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-03 12:39:41 +0600

    Add metaData to homePage and call Reservation component

--- src/app/(mainLayout)/home/page.tsx
+++ src/app/(mainLayout)/home/page.tsx
@@ -1,10 +1,16 @@
-import FormDatePicker from "@/components/Form/FormDatePicker";
+import Reservation from "@/components/Reservation/Reservation";
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "DriveEase | Home",
+  description: "Home page of DriveEase",
+};
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <FormDatePicker />
+      <Reservation />
     </div>
   );
 };
